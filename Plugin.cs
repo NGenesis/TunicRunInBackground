46732d07d4d38beb022289ceac7991c43e8eede1
NGenesis <NGenesis@users.noreply.github.com>
2025-05-05 03:46:04 +0100

    Initial commit.

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace TunicRunInBackground
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        internal static new ManualLogSource Log;
+
+        public override void Load()
+        {
+            Log = base.Log;
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            Application.runInBackground = true;
+        }
+    }
+}
